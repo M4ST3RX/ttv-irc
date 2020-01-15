6fd120447041826b80f2f030db4f55ea18aa6908
Martin Kun <martin.kun@parall.ax>
2020-01-15 17:39:36 +0000

    Added camel case helper function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+String.prototype.toCamelCase = function() {
+	let parts = this.split('-');
+	let output = '';
+	parts.forEach((value, index) => {
+		if(index === 0) {
+			output += value;
+		} else {
+			output += value.substring(0, 1).toUpperCase() + value.slice(1);
+		}
+	});
+	return output;
+}
+
 module.exports = {
 	'IRC': require('./Models/IRC'),
 	'IRCConnection': require('./Models/IRCConnection'),
